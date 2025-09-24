1a02b0aa290a2343c6e73432889e9982f79b14a8
Simon de Vlieger <supakeen@redhat.com>
2025-09-24 09:31:56 +0200

    test: check expected fedora types
    Ensure that the image types configured in Fedora pungi instances are
    available in the list of image types.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- test/test_container.py
+++ test/test_container.py
@@ -212,7 +212,6 @@ def test_container_has_expected_images_centos(build_container):
         "list",
     ], text=True)
 
-
     type_arch = {
         "tar": ["aarch64", "x86_64", "ppc64le", "s390x"],
         "qcow2": ["aarch64", "x86_64", "ppc64le", "s390x"],
@@ -225,6 +224,35 @@ def test_container_has_expected_images_centos(build_container):
     }
 
     for distro in ["centos-9", "centos-10"]:
+        for type_, arches in type_arch.items():
+            for arch in arches:
+                assert f"{distro} type:{type_} arch:{arch}" in output
+
+
+def test_container_has_expected_images_fedora(build_container):
+    """
+    Ensure that image types that are built in by Fedora are available
+    and do not disappear from the list. See:
+    https://pagure.io/pungi-fedora/blob/main/f/fedora.conf
+    and
+    https://pagure.io/fedora-iot/pungi-iot/blob/main/f/fedora-iot.conf
+    """
+
+    output = subprocess.check_output([
+        "podman", "run",
+        "--privileged",
+        build_container,
+        "list",
+    ], text=True)
+
+    type_arch = {
+        "minimal-raw-xz": ["aarch64"],
+        "iot-raw-xz": ["x86_64", "aarch64"],
+        "iot-installer": ["x86_64", "aarch64"],
+        "iot-simplified-installer": ["x86_64", "aarch64"],
+    }
+
+    for distro in ["fedora-42", "fedora-43", "fedora-44"]:
         for type_, arches in type_arch.items():
             for arch in arches:
                 assert f"{distro} type:{type_} arch:{arch}" in output
