osbuild/bootc-image-builder

Use blueprint filesystem customizations to create partitions
This also allows the root filesystem size to be increased if the default
of 10GiB isn't enough.

bib/cmd/bootc-image-builder/image.go
@@ -43,8 +43,9 @@ type ManifestConfig struct {
 	// Use a local container from the host rather than a repository
 	Local bool
 
-	// Only the "/" filesystem size is configured here right now
-	Filesystems []blueprint.FilesystemCustomization
+	// Only the "/" filesystem size is configured here right now, computed
+	// from the base image size.
+	DefaultFilesystems []blueprint.FilesystemCustomization
 }
 
 func Manifest(c *ManifestConfig) (*manifest.Manifest, error) {
@@ -113,7 +114,18 @@ func manifestForDiskImage(c *ManifestConfig, rng *rand.Rand) (*manifest.Manifest
 	if !ok {
 		return nil, fmt.Errorf("pipelines: no partition tables defined for %s", c.Architecture)
 	}
-	pt, err := disk.NewPartitionTable(&basept, c.Filesystems, DEFAULT_SIZE, disk.RawPartitioningMode, nil, rng)
+
+	filesystems := customizations.GetFilesystems()
+	if len(filesystems) == 0 {
+		filesystems = c.DefaultFilesystems
+	}
+
+	pt, err := disk.NewPartitionTable(&basept,
+		filesystems,
+		DEFAULT_SIZE,
+		disk.RawPartitioningMode,
+		nil,
+		rng)
 	if err != nil {
 		return nil, err
 	}

osbuild/image-builder-cli

cmd/bootc-image-builder/image.go
@@ -137,8 +137,12 @@ func manifestForDiskImage(c *ManifestConfig, rng *rand.Rand) (*manifest.Manifest
 		}
 		rootFS.Type = c.RootFSType
 	}
+	filesystems := customizations.GetFilesystems()
+	if len(filesystems) == 0 {
+		filesystems = c.Filesystems
+	}
 
-	pt, err := disk.NewPartitionTable(&basept, c.Filesystems, DEFAULT_SIZE, disk.RawPartitioningMode, nil, rng)
+	pt, err := disk.NewPartitionTable(&basept, filesystems, DEFAULT_SIZE, disk.RawPartitioningMode, nil, rng)
 	if err != nil {
 		return nil, err
 	}