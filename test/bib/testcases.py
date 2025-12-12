mvo5/bootc-image-builder

bib: remove usage of `BootcLegacyDiskImage` (for qemu-9.1)
This commit enables bib to work with the qemu-9.1+openat2 patches
as proposed in https://src.fedoraproject.org/rpms/qemu/pull-request/70.

With the qemu-user-statuc-aarch64 rpm produced by `fedpkg mockbuild`
from this package the cross-arch test build works again.
```
$ sudo PYTHONPATH=. pytest './test/test_build.py::test_image_boots[quay.io/centos-bootc/centos-bootc:stream9,raw,CentOS Stream 9 ({arch}),arm64]' -s -vv
============================= test session starts ==============================
platform linux -- Python 3.12.7, pytest-7.4.3, pluggy-1.3.0 -- /usr/bin/python3
cachedir: .pytest_cache
rootdir: /home/mvogt/devel/osbuild/bootc-image-builder
configfile: pytest.ini
plugins: repeat-0.9.3, xdist-3.5.0
collected 1 item

test/test_build.py::test_image_boots[quay.io/centos-bootc/centos-bootc:stream9,raw,CentOS Stream 9 ({arch}),arm64] [1/2] STEP 1/9: FROM registry.fedoraproject.org/fedora:40 AS builder
...
PASSEDChecking disk usage for /var/tmp/bib-tests/shared0/68b20145da2cd3f2/image/disk.raw
NOTE: disk usage after /var/tmp/bib-tests/shared0/68b20145da2cd3f2/image/disk.raw: 712472.71936 / 1998694.907904
Untagged: quay.io/centos-bootc/centos-bootc:stream9
Deleted: 0de1f90b11cbe7e2768101f6c55b4dd9841c13a2f87c8d9c177aea74ade88050

============================= slowest 10 durations =============================
1033.54s setup    test/test_build.py::test_image_boots[quay.io/centos-bootc/centos-bootc:stream9,raw,CentOS Stream 9 ({arch}),arm64]
67.62s call     test/test_build.py::test_image_boots[quay.io/centos-bootc/centos-bootc:stream9,raw,CentOS Stream 9 ({arch}),arm64]
2.30s teardown test/test_build.py::test_image_boots[quay.io/centos-bootc/centos-bootc:stream9,raw,CentOS Stream 9 ({arch}),arm64]
======================== 1 passed in 1103.58s (0:18:23) ========================
```

test/testcases.py
@@ -77,11 +77,8 @@ def gen_testcases(what):  # pylint: disable=too-many-return-statements
         ]
         # do a cross arch test too
         if platform.machine() == "x86_64":
-            # TODO: re-enable once
-            # https://github.com/osbuild/bootc-image-builder/issues/619
-            # is resolved
-            # test_cases.append(
-            #    TestCaseCentos(image="raw", target_arch="arm64"))
+            test_cases.append(
+                TestCaseCentos(image="raw", target_arch="arm64"))
             pass
         elif platform.machine() == "arm64":
             # TODO: add arm64->x86_64 cross build test too

osbuild/image-builder-cli

test/bib/testcases.py
@@ -104,12 +104,8 @@ def gen_testcases(what):  # pylint: disable=too-many-return-statements
         ]
         # do a cross arch test too
         if platform.machine() == "x86_64":
-            # TODO: re-enable once
-            # https://github.com/osbuild/bootc-image-builder/issues/619
-            # is resolved
-            # test_cases.append(
-            #    TestCaseC9S(image="raw", target_arch="arm64"))
-            pass
+            test_cases.append(
+                TestCaseC9S(image="raw", target_arch="arm64"))
         elif platform.machine() == "arm64":
             # TODO: add arm64->x86_64 cross build test too
             pass