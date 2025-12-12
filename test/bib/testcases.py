mvo5/bootc-image-builder

test: run the centos-9 test with an attached terminal
This commit changes the centos-9 test to run with `podman -t` so
that we have a test-case that uses the `terminal` progress. This
is prompted by:
a) we have no integration test currently that uses the terminal
   progress for the full build
b) a konflux failure/memory leak that showed because there the
   test is run with `-t`

test/testcases.py
@@ -27,6 +27,8 @@ class TestCase:
     disk_config: str = ""
     # use librepo for the downloading
     use_librepo: bool = False
+    # podman_terminal enables the podman -t option to get progress
+    podman_terminal: bool = False
 
     def bib_rootfs_args(self):
         if self.rootfs:
@@ -61,6 +63,7 @@ class TestCaseC9S(TestCase):
         "BIB_TEST_BOOTC_CONTAINER_TAG",
         "quay.io/centos-bootc/centos-bootc:stream9")
     use_librepo: bool = True
+    podman_terminal: bool = True
 
 
 @dataclasses.dataclass

osbuild/image-builder-cli

test/bib/testcases.py
@@ -27,6 +27,8 @@ class TestCase:
     disk_config: str = ""
     # use librepo for the downloading
     use_librepo: bool = False
+    # podman_terminal enables the podman -t option to get progress
+    podman_terminal: bool = False
 
     def bib_rootfs_args(self):
         if self.rootfs:
@@ -61,6 +63,7 @@ class TestCaseC9S(TestCase):
         "BIB_TEST_BOOTC_CONTAINER_TAG",
         "quay.io/centos-bootc/centos-bootc:stream9")
     use_librepo: bool = True
+    use_terminal: bool = True
 
 
 @dataclasses.dataclass