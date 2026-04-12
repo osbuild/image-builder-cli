882fd9d94295bb9fc1f20f66c0e21c3566636ae4
witcher <roman.web.kuznietsov@gmail.com>
2026-04-12 20:30:52 +0000

    basic filesystem checks

--- pkg/validation/customization.go
+++ pkg/validation/customization.go
@@ -10,11 +10,20 @@ import (
 func firewall(imageFormat ImageModel.CLIOutputFormat, config *blueprint.FirewallCustomization) []string {
 	warnings := []string{}
 	if imageFormat == ImageModel.FormatGCE || imageFormat == ImageModel.FormatOpenStack {
-		message := "The Google and OpenStack templates explicitly disable the firewall for their environment. This cannot be overridden by the blueprint."
-		warnings = append(warnings, message)
+		m := "The Google and OpenStack templates explicitly disable the firewall for their environment. This cannot be overridden by the blueprint."
+		warnings = append(warnings, m)
 	}
 
 	return warnings
+}
+
+func filesystem(imageFormat ImageModel.CLIOutputFormat, config []blueprint.FilesystemCustomization) (error, []string) {
+	warnings := []string{}
+	if imageFormat == ImageModel.FormatImageInstaller || imageFormat == ImageModel.FormatEdgeInstaller || imageFormat == ImageModel.FormatEdgeSimplifiedInstaller {
+		return errors.New("Filesystem customizations are currently not supported for the following image types: image-installer, edge-installer, edge-simplified-installer"), warnings
+	}
+
+	return nil, warnings
 }
 
 // This would be more of a "policy"-based validation. Check very specific cases. Not sure if I love it, would be cool to figure out a more declarative way
@@ -23,6 +32,24 @@ func CustomizationConflicts(imageFormat ImageModel.CLIOutputFormat, config *blue
 	if config.Firewall != nil {
 		messages := firewall(imageFormat, config.Firewall)
 		warnings = append(warnings, messages...)
+		if failOnWarning && len(messages) > 0 {
+			return errors.New(messages[0]), warnings
+		}
+	}
+
+	if config.Filesystem != nil {
+		if config.Disk != nil {
+			m := "Filesystem and disk customizations are mutually exclusive."
+			warnings = append(warnings, m)
+			if failOnWarning {
+				return errors.New(m), warnings
+			}
+		}
+		err, messages := filesystem(imageFormat, config.Filesystem)
+		warnings = append(warnings, messages...)
+		if err != nil {
+			return err, warnings
+		}
 		if failOnWarning && len(messages) > 0 {
 			return errors.New(messages[0]), warnings
 		}
