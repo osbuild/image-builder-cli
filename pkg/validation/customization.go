8d9cd5fe1916e12711df2d43abfbbbc024e3de6c
witcher <roman.web.kuznietsov@gmail.com>
2026-04-12 18:52:32 +0000

    invert validation logic checks - start from customization fields and check things relevant to them

--- pkg/validation/customization.go
+++ pkg/validation/customization.go
@@ -7,18 +7,25 @@ import (
 	ImageModel "github.com/osbuild/image-builder-cli/pkg/image_model"
 )
 
+func firewall(imageFormat ImageModel.CLIOutputFormat, config *blueprint.FirewallCustomization) []string {
+	warnings := []string{}
+	if imageFormat == ImageModel.FormatGCE || imageFormat == ImageModel.FormatOpenStack {
+		message := "The Google and OpenStack templates explicitly disable the firewall for their environment. This cannot be overridden by the blueprint."
+		warnings = append(warnings, message)
+	}
+
+	return warnings
+}
+
 // This would be more of a "policy"-based validation. Check very specific cases. Not sure if I love it, would be cool to figure out a more declarative way
 func CustomizationConflicts(imageFormat ImageModel.CLIOutputFormat, config *blueprint.Customizations, failOnWarning bool) (error, []string) {
 	warnings := []string{}
-	if imageFormat == ImageModel.FormatGCE || imageFormat == ImageModel.FormatOpenStack {
-		if config.Firewall != nil {
-			message := "The Google and OpenStack templates explicitly disable the firewall for their environment. This cannot be overridden by the blueprint."
-			if failOnWarning {
-				return errors.New(message), warnings
-			}
-			warnings = append(warnings, message)
+	if config.Firewall != nil {
+		messages := firewall(imageFormat, config.Firewall)
+		warnings = append(warnings, messages...)
+		if failOnWarning && len(messages) > 0 {
+			return errors.New(messages[0]), warnings
 		}
-
 	}
 
 	return nil, warnings
