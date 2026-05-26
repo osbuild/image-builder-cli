osbuild/image-builder-cli

commit 8a1411cd469ae028cd607a322be113831ca926ca

many: update all refs to Fedora 43
It's now GA, so let's switch to it.

Containerfile
@@ -1,4 +1,4 @@
-FROM registry.fedoraproject.org/fedora:42 AS builder
+FROM registry.fedoraproject.org/fedora:43 AS builder
 RUN dnf install -y git-core golang gpgme-devel libassuan-devel && mkdir -p /build/
 ARG GOPROXY=https://proxy.golang.org,direct
 RUN go env -w GOPROXY=$GOPROXY
@@ -11,7 +11,7 @@ WORKDIR /build
 # b) we don't really need it
 RUN CGO_ENABLED=0 go build -tags "containers_image_openpgp exclude_graphdriver_btrfs exclude_graphdriver_devicemapper" ./cmd/image-builder
 
-FROM registry.fedoraproject.org/fedora:41
+FROM registry.fedoraproject.org/fedora:43
 
 # podman mount needs this
 RUN mkdir -p /etc/containers/networks

cmd/image-builder/main_test.go
@@ -250,7 +250,7 @@ func TestManifestIntegrationOstreeSmoke(t *testing.T) {
 		"manifest",
 		"iot-raw-xz",
 		"--arch=x86_64",
-		"--distro=fedora-42",
+		"--distro=fedora-43",
 		"--ostree-url=" + strings.SplitN(string(body), "\n", 2)[0],
 		"--ostree-ref=fedora/stable/x86_64/iot",
 	})
@@ -282,7 +282,7 @@ func TestManifestIntegrationOstreeSmokeErrors(t *testing.T) {
 	baseArgs := []string{
 		"manifest",
 		"--arch=x86_64",
-		"--distro=fedora-42",
+		"--distro=fedora-43",
 	}
 
 	for _, tc := range []struct {

cmd/image-builder/upload_test.go
@@ -242,7 +242,7 @@ func TestBuildAndUploadFedoraWithAWSMock(t *testing.T) {
 		"--aws-bucket=aws-bucket-2",
 		"--aws-ami-name=aws-ami-3",
 		"server-ami",
-		"--distro=fedora-42",
+		"--distro=fedora-43",
 	})
 	defer restore()
 

test/test_build_cross.py
@@ -24,8 +24,8 @@ def test_build_cross_builds(tmp_path, build_container, arch):
         "--progress=verbose",
         "--output-dir=/output",
         "container",
-        "--distro", "fedora-41",
+        "--distro", "fedora-43",
         # selecting a foreign arch here automatically triggers a cross-build
         f"--arch={arch}",
     ], text=True)
-    assert os.path.exists(output_dir / f"fedora-41-container-{arch}.tar")
+    assert os.path.exists(output_dir / f"fedora-43-container-{arch}.tar")

commit 7c08e78ea98a2da4c42ef36802a84d37a31606d3

Containerfile*: correct base image reference in tag
Fixes: 8a1411cd4 ("many: update all refs to Fedora 43")

Containerfile
@@ -33,5 +33,5 @@ VOLUME /var/lib/containers/storage
 LABEL description="This tools allows to build and deploy disk-images."
 LABEL io.k8s.description="This tools allows to build and deploy disk-images."
 LABEL io.k8s.display-name="Image Builder"
-LABEL io.openshift.tags="base fedora40"
+LABEL io.openshift.tags="base fedora43"
 LABEL summary="A container to create disk-images."

Containerfile.bib
@@ -29,5 +29,5 @@ VOLUME /var/lib/containers/storage
 LABEL description="This tools allows to build and deploy disk-images from bootc container inputs."
 LABEL io.k8s.description="This tools allows to build and deploy disk-images from bootc container inputs."
 LABEL io.k8s.display-name="Bootc Image Builder"
-LABEL io.openshift.tags="base fedora42"
+LABEL io.openshift.tags="base fedora43"
 LABEL summary="A container to create disk-images from bootc container inputs"